68a0d49d32aaedf9288a5825a23f2b9fe2db9e4f
tms320 <stacked-software@yandex.ru>
2018-05-27 16:29:10 +0300

    Added 'error.go' with errors related functions

---
+++ error.go
@@ -0,0 +1,26 @@
+package yagolib
+
+import (
+	"fmt"
+	"os"
+)
+
+func Exit(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func StdErrPrint(a ...interface{}) {
+	fmt.Fprint(os.Stderr, a)
+}
+
+func StdErrPrintln(a ...interface{}) {
+	fmt.Fprintln(os.Stderr, a)
+}
+
+func StdErrPrintf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format, a)
+}
