5500c92996b58b2224b7869602dc84e39b2cfda5
tms320 <stacked-software@yandex.ru>
2018-06-11 21:37:37 +0300

    Added function to normalize file path and expand '~' to user home directory

--- file.go
+++ file.go
@@ -2,14 +2,29 @@ package yagolib
 
 import (
 	"os"
+	"os/user"
 	"path/filepath"
+	"strings"
 )
 
 func IsFileExists(path string) bool {
 	// https://stackoverflow.com/questions/12518876/how-to-check-if-a-file-exists-in-go
-	path, _ = filepath.Abs(path)
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
-		return true
+	if path, err := NormalizePath(path); err == nil {
+		if _, err := os.Stat(path); !os.IsNotExist(err) {
+			return true
+		}
 	}
 	return false
+}
+
+func NormalizePath(path string) (string, error) {
+	if strings.HasPrefix(path, "~/") {
+		usr, err := user.Current()
+		if err == nil {
+			path = filepath.Join(usr.HomeDir, path[1:])
+		} else {
+			return path, err
+		}
+	}
+	return filepath.Abs(path)
 }
